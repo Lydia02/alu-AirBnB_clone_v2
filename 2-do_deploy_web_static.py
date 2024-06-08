e4d9fbf396f92769fe5dc9c900c45ed47f5c37cb
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-06-08 03:00:23 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to web servers and verifies deployment
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import put, run, env, local
+from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['100.27.227.145', '54.226.121.12']
 
+
 def do_deploy(archive_path):
-    """distributes an archive to the web servers and verifies deployment"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
         file_n = archive_path.split("/")[-1]
@@ -23,18 +25,6 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        check_deployment(no_ext)
         return True
-    except Exception as e:
-        print(f"Deployment failed: {e}")
+    except:
         return False
-
-def check_deployment(no_ext):
-    """Verifies if the deployment was successful by checking the HTTP status code of the deployed page"""
-    for host in env.hosts:
-        response = run('curl -o /dev/null -s -w "%{http_code}" http://{}:80/hbnb_static/0-index.html'.format(host))
-        print(f"HTTP status for {host}: {response}")
-        if response != '200':
-            print(f"Deployment check failed on {host}")
-        else:
-            print(f"Deployment successful on {host}")
