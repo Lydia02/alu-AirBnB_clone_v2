734a5dddc2f32f117c5b63b0251354a371e6e503
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-06-12 23:23:52 +0100

    Update 2-c_route.pysd

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" Script that runs a flask application"""
+""" Write a script that starts a Flask web application,
+Web application is listening on 0.0.0.0, port 5000 """
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/")
 def hello_hbnb():
-    """ function that return " hello hbnb" """
-    return 'Hello HBNB!'
+    """ Displaying Hello HBNB! """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
-    """ function that displays hbnb!"""
-    return 'HBNB'
+    """ Display HBNB! """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ function that returns "C" followed by the value of the text"""
-    return 'C {}'.format(text.replace('_', ''))
+@app.route("/c/<text>")
+def text_c(text):
+    """ Display C """
+    return "C {}".format(text.replace("_", " "))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    app.url_map.strict_slashes = False
