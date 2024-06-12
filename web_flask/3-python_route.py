76c17a09d12e6e3008b3e44de8c14e2b7f01c85a
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-06-12 23:25:08 +0100

    Update 3-python_route.pycvz

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-""" Script that runs a flask application"""
+""" Write a script that starts a Flask web application,
+Web application is listening on 0.0.0.0, port 5000 """
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/")
 def hello_hbnb():
-    """ function that return " hello hbnb" """
-    return 'Hello HBNB!'
+    """ Displaying Hello HBNB! """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
-    """ function that displays hbnb!"""
-    return 'HBNB'
+    """ Display HBNB! """
+    return "HBNB"
 
 
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ function that returns "C" followed by the value of the text"""
-    return 'C {}'.format(text.replace('_', ''))
-
-
-@app.route('/pythons', strict_slashes=False)
-@app.route('/pythons/<text>', strict_slashes=False)
-def python_is_cool(text = 'is  cool'):
-    """ function that returns python followed by the value of the text"""
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route("/c/<text>")
+def text_c(text):
+    """ Display C """
+    return "C {}".format(text.replace("_", " "))
 
 
+@app.route("/python/")
+@app.route("/python/<text>")
+def text_py(text="is cool"):
+    """ Display Python """
+    return "Python {}".format(text.replace("_", " "))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    app.url_map.strict_slashes = False
